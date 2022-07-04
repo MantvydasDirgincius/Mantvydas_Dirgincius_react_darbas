2b847f9a909045c89f4b2ce73bf4fcd7af05cfde
Mantvydas <mantvydas.dirgincius@icloud.comm>
2022-07-04 13:58:58 +0300

    navigation authcontext and registration funcion working

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+import css from './Navigation.module.css';
 
 function Navigation() {
-  return <div>Navigation</div>;
+  return (
+    <header className={css.header}>
+      <div className={`container ${css.flexingNav}`}>
+        <Link to={'/'}>
+          <img
+            className={css.img}
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2048px-Google_%22G%22_Logo.svg.png'
+            alt='google logo'
+          />
+        </Link>
+        <nav>
+          <NavLink to='/' className={css.navLink}>
+            Home
+          </NavLink>
+
+          <NavLink to='/login' className={css.navLink}>
+            Login
+          </NavLink>
+
+          <NavLink to='/login' className={css.navLink}>
+            log out
+          </NavLink>
+
+          <NavLink to='/register' className={css.navLink}>
+            Register
+          </NavLink>
+          <NavLink to='/Add' className={css.navLink}>
+            Add
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Navigation;
