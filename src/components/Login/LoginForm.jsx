9db47c4dcfab81cb36690df46b74a81aaddd2f8e
Mantvydas <mantvydas.dirgincius@icloud.comm>
2022-07-04 15:03:41 +0300

    login page works

---
+++ src/components/Login/LoginForm.jsx
@@ -0,0 +1,70 @@
+import { useFormik } from 'formik';
+import { useContext } from 'react';
+
+import * as Yup from 'yup';
+import { AuthContext } from '../../store/authContext';
+import { doPostRequest } from '../../utils';
+import css from './LoginForm.module.css';
+
+const initValues = {
+  email: '',
+  password: '',
+};
+function LoginForm({ onSuccessLogin }) {
+  const { login } = useContext(AuthContext);
+  const formik = useFormik({
+    initialValues: initValues,
+    validationSchema: Yup.object({
+      email: Yup.string().email().required('Required'),
+      password: Yup.string().min(4).required('Required'),
+    }),
+    onSubmit: async (values) => {
+      const result = await doPostRequest(
+        'https://autumn-delicate-wilderness.glitch.me/v1/auth/login',
+        values
+      );
+      console.log('result ===', result);
+      console.log('result.token ===', result.token);
+      //   if (!result.response.status === 400) {
+      //     console.log('False token');
+      //     return;
+      //   }
+
+      onSuccessLogin();
+
+      login(result.token);
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <input
+        onBlur={formik.handleBlur}
+        onChange={formik.handleChange}
+        value={formik.values.email}
+        className={formik.touched.email && formik.errors.email ? css.errorInput : ''}
+        name='email'
+        type='text'
+        placeholder='Your Email'
+      />
+      {formik.touched.email && formik.errors.email && (
+        <p className={css.errorMsg}>{formik.errors.email}</p>
+      )}
+      <input
+        onBlur={formik.handleBlur}
+        onChange={formik.handleChange}
+        value={formik.values.password}
+        className={formik.touched.password && formik.errors.password ? css.errorInput : ''}
+        name='password'
+        type='password'
+        placeholder='Your password'
+      />
+      {formik.touched.password && formik.errors.password && (
+        <p className={css.errorMsg}>{formik.errors.password}</p>
+      )}
+      <button type='submit'>Login</button>
+    </form>
+  );
+}
+
+export default LoginForm;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import LoginForm from '../../components/Login/LoginForm';
 
 function Login() {
-  return <div>Login</div>;
+  const history = useHistory();
+  function handleSuccessLogin() {
+    history.replace('/');
+  }
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginForm onSuccessLogin={handleSuccessLogin} />
+    </div>
+  );
 }
 
 export default Login;
