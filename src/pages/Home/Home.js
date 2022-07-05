92fcc0fbf1239695677d976f6cb369ef8dbd8b18
Mantvydas <mantvydas.dirgincius@icloud.comm>
2022-07-05 12:30:18 +0300

    home page working

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import HomeComp from '../../components/HomeComp/HomeComp';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <HomeComp />
+    </div>
+  );
 }
 
 export default Home;
